35e24426c92d902bd7243f822d85ab98e666c011
SanketRakshe <sanketrakshe07@gmail.com>
2024-10-23 12:20:32 +0530

    FEAT: added a fallback machanism after failing the request the respective response will goes to the client

--- src/main/java/com/example/Product_Service/controller/ProductController.java
+++ src/main/java/com/example/Product_Service/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.example.Product_Service.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,29 +16,35 @@ import io.github.resilience4j.retry.annotation.Retry;
 @RestController
 public class ProductController {
 	
+	Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+	private static Long counter = 0l;
+	
 	@Autowired
 	private ProductConfig productConfig;
-	
+
 	@Autowired
 	private Environment environment;
-	
-	@Retry(name = "get-product")
+
+	@Retry(name = "get-product", fallbackMethod = "getProductFallBack")
 	@GetMapping("/product_info/{productName}")
 	public Product getProduct(@PathVariable String productName) {
+
+		logger.info("ProductController -> getProduct - " + (++counter)  + ", productName - " + productName);
 		
-		System.out.println("/product_info -----------------");
 		if (productName == null || productName.equalsIgnoreCase("BAD_PRODUCT")) {
 			throw new RuntimeException("Bad Product");
 		}
-		
-		System.out.println("Port - "
-				+ environment.getProperty("local.server.port"));
-		
-		return productConfig.getProducts()
-				.stream()
-				.filter(
-						 p -> p.getName().equalsIgnoreCase(productName))
-				.findFirst()
+
+		logger.info("Product Details  - " + productConfig.getProducts() );
+
+		return productConfig.getProducts().stream().filter(p -> p.getName().equalsIgnoreCase(productName)).findFirst()
 				.orElseGet(() -> new Product());
+	}
+
+	private Product getProductFallBack(Exception ex) {
+		Product product = new Product();
+		product.setName("Dummy Product");
+		return product;
 	}
 }
